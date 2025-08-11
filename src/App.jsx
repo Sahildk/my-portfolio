0cad9142df2829262b28073e51c33a731ceaa600
Sahil Deore <sahildeore1234@gmail.com>
2025-08-11 12:46:04 +0530

    Hero Section Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-
+import Hero from "./components/Hero"
 const App = () => {
   return (
     <div className="overflow-x-hidden text-stone-300 antialiased">
@@ -12,6 +12,7 @@ const App = () => {
       </div>
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
     </div>
   )
