99b2b0cc258ca2f37a7449d4cecc20ea8eeb7c70
Sahil D <114382046+Sahildk@users.noreply.github.com>
2025-08-13 01:39:43 +0530

    framer motion added

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,8 +1,8 @@
+import { motion } from "framer-motion"
 import { FaNodeJs } from "react-icons/fa6"
 import { RiReactjsLine, RiTailwindCssFill } from "react-icons/ri"
 import { SiMongodb } from "react-icons/si"
 import { TbBrandNextjs } from "react-icons/tb"
-import { animate, motion } from "framer-motion"
 
 const iconVariants = duration => ({
   initial: { y: -10 },
@@ -20,24 +20,60 @@ const iconVariants = duration => ({
 const Technologies = () => {
   return (
     <div className="pb-24">
-      <motion.h2 className="my-20 text-center text-4xl">Technologies</motion.h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+        >
           <RiReactjsLine className="text-7xl text-[#61DAFB]" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+          className="p-4"
+        >
           <TbBrandNextjs className="text-7xl" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+          className="p-4"
+        >
           <RiTailwindCssFill className="text-7xl text-[#38BDF8]" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2)}
+          className="p-4"
+        >
           <FaNodeJs className="text-7xl text-[#339933]" />
-        </div>
-        <div className="p-4">
+        </motion.div>
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(6)}
+          className="p-4"
+        >
           <SiMongodb className="text-7xl text-[#47A248]" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -3,8 +3,7 @@ import project2 from "../assets/projects/project-2.webp"
 import project3 from "../assets/projects/project-3.webp"
 import project4 from "../assets/projects/project-4.webp"
 
-export const HERO_CONTENT = `I am a full-stack web developer with 3 years of experience creating scalable, high-performance web applications. Skilled in React, Next.js, Tailwind CSS, Node.js, MongoDB, and MySQL, I have delivered real-world projects including hotel and café websites. Currently pursuing my B.Tech in IT, I combine solid coding fundamentals with AI tools like GitHub Copilot to craft innovative solutions and exceptional user experiences.`
-
+export const HERO_CONTENT = `I am a full-stack web developer with 3 years of experience, I build fast, scalable, and visually polished web applications using React, Next.js, Tailwind CSS, Node.js, MongoDB, and MySQL. Delivered successful projects such as responsive hotel and café websites, improving client visibility and engagement. Known for writing clean, maintainable code and optimizing performance. I also leverage AI-powered coding assistants and LLMs (GitHub Copilot, ChatGPT) to accelerate development, enhance code quality, and reduce debugging time.`
 export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`
 
 export const EXPERIENCES = [
